e09e8ae279ca31bead94efcc8d7051311de30975
dan <875126243@qq.com>
2024-03-04 17:54:12 +0800

    fix update c#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    internal class Prggram
+    {
+        public static int _number = 0;
+        static void Main(string[] arggs)
+        {
+            int a = 10;
+            int b=  Test(a);
+            int n = 100;
+            int m = 200;
+            var sum = n + m;
+            Console.WriteLine(n);
+            Console.WriteLine(m);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+
+        public static int Test(int n)
+        {
+            return ++n;
+        }
+
+        public static void ceshi()
+        {
+            string a = "anbc";
+
+        }
+
+
+    }
+}
