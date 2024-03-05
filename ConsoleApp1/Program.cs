76296a8183b252e0d9529eba2437e89ce3e24f4f
dan <875126243@qq.com>
2024-03-05 15:06:41 +0800

    fix  update  code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,30 +11,73 @@ namespace HelloWorldCS
         public static int _number = 0;
         static void Main(string[] arggs)
         {
-            int a = 10;
-            int b=  Test(a);
-            int n = 100;
-            int m = 200;
-            var sum = n + m;
-            bool d = false;
-
-            Console.WriteLine(d);
-            Console.WriteLine(n);
-            Console.WriteLine(m);
-            Console.WriteLine(sum);
+            //变量类型
+            IntDemo();
+            StringDemo();
+            BoolDemo();
+            Test();
             Console.ReadKey();
         }
-
-        public static int Test(int n)
+        
+        public static void IntDemo()
         {
-            return ++n;
+            Console.WriteLine("int 使用案例");
+            int a = 10;
+            int b = 20;
+            Console.WriteLine(a + b);
+
+            int n1 = 20;
+            Console.WriteLine(n1);
+            int n2 = 30;
+            Console.WriteLine(n2);
+
+            int temp = 0;
+            Console.WriteLine(temp);
+            temp = n1; //20
+            Console.WriteLine(temp);
+            n1 = n2; //30
+            Console.WriteLine(n2);
+            Console.WriteLine(n1);
+            n2 = temp; //20
+            Console.WriteLine(n2);
+            
         }
 
-        public static void Ceshi()
+        public static void StringDemo()
         {
+            Console.WriteLine("string 使用案例");
             string a = "anbc";
             Console.WriteLine(a);
+        }
 
+       public static void BoolDemo()
+       {
+            Console.WriteLine("bool 使用案例");
+            bool a = false;
+            bool b = true;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+       }
+
+        public static void Test()
+        {
+            Console.WriteLine("请输入一个数字");
+            string str =  Console.ReadLine();
+            if (IsNumeric(str))
+            {
+                int mum = int.Parse(str) + 20;
+                Console.WriteLine(mum);
+            }
+            else
+            {
+                string str1 = str + 20.ToString();
+                Console.WriteLine(str1);
+            }
+        }
+
+        static bool IsNumeric(string str)
+        {
+            return double.TryParse(str, out _);
         }
 
 
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090b06d422a0f7c98ac97604d092c8e2dea84a9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ae47932c253e955c81ac64d67e884d0db87382")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
