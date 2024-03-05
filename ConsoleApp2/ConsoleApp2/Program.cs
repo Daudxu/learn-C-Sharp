e653cc3879958a7b8dbd7df975694c47d567c86a
dan <875126243@qq.com>
2024-03-05 16:50:06 +0800

    fix update code

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -13,12 +13,23 @@ namespace HelloWorldCS
         {
             Console.WriteLine("请输入你的姓名：");
             string name = Console.ReadLine();
+            bool isZhangsan = name == "张三";
+            Console.WriteLine(isZhangsan);
+            if(name == "zhangsan")
+            {
+                name = "法外狂赌" + name;
+            }
 
             Console.WriteLine("请输入你的年龄：");
             string age = Console.ReadLine();
 
             Console.WriteLine("请输入你的爱好：");
             string hobby = Console.ReadLine();
+            
+            //重新赋值
+        /*  name = "lisi";
+            age = "666";
+            hobby = "read book";*/
 
             Console.WriteLine("");
             Console.WriteLine("");
@@ -28,6 +39,5 @@ namespace HelloWorldCS
             Console.WriteLine("请输入你的爱好：" + hobby);
             Console.ReadKey();
         }
-
     }
 }
