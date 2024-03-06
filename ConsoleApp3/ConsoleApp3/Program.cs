4af26f8827e6b34cb33620be5d352ece602fc543
dan <875126243@qq.com>
2024-03-06 17:28:54 +0800

    fix update  code

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+            bool isCheck = true;
+
+            for (; isCheck;)
+            {
+                Console.WriteLine("请输入你的年龄!");
+                string str = Console.ReadLine();
+
+                try
+                {
+                    age = int.Parse(str);
+                    isCheck = false;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("请输入一个正确的年龄!");
+              /*      throw;*/
+                }
+            }
+           
+        }
+    }
+}
