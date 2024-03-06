d81f1faa6de2dfd501c4be224fc3ca333ddf36fc
dan <875126243@qq.com>
2024-03-06 18:35:38 +0800

    fix update code

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Hero sa = new Hero();
+            sa.xingbie = "man";
+            sa.ShowInfo();
+         /*   Hero.staitcInfo = "111";*/
+            Console.WriteLine(Hero.staitcInfo);
+        }
+    }
+
+    class Hero
+    {
+        public string mingzi;
+        public string xingbie;
+        public int nianling;
+        public int shanghai;
+        public int xueliang;
+        public static string staitcInfo = "666";
+        public void ShowInfo()
+        {
+            Console.WriteLine(xingbie);
+        }
+    }
+}
