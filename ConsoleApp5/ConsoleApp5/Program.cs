be5f160637f3d539bbe4ce727a6444d5e6f21e0f
dan <875126243@qq.com>
2024-03-07 11:16:04 +0800

    fix update code

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var PersonObj = new Person();
+            PersonObj.Name = "张三";
+            PersonObj.Sex = "男";
+            PersonObj.Age = 18;
+
+            Console.WriteLine("姓名："+ PersonObj.Name);
+            Console.WriteLine("性别："+ PersonObj.Sex);
+            Console.WriteLine("年龄："+ PersonObj.Age);
+            
+            Console.ReadKey();
+        }
+
+        static void GetUserInfo()
+        {
+
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+
+        public string Sex { get; set; }
+
+        public int Age { get; set; }
+    }
+}
